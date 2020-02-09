8290a59f6a3ae2c9032237fee88f346bb5dbfac6
mxstrong <mantas.ptakauskas@gmail.com>
2020-02-09 12:41:09 +0200

    Customise greeting

--- iremintas_pasisveikinimas.cpp
+++ iremintas_pasisveikinimas.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 using namespace std;
 
+bool isWoman(string name) {
+  int lastLetterNumber = name.length() - 1;
+  if (name[lastLetterNumber] == 'a' || name[lastLetterNumber] == 'e') {
+    return true;
+  }
+  return false;
+}
+
 int main() {
   cout << "Iveskite varda" << endl;
   string name;
   cin >> name;
-
-  string greeting = "Sveikas, ";
+  string greeting = "";
+  if (isWoman(name)) {
+    greeting = "Sveika, ";
+  }
+  else {
+    greeting = "Sveikas, ";
+  }
+  
   int length = 2 + greeting.length() + name.length() + 2;
   string first = "", second = "", fourth = "", fifth = "";
   for (int i = 0; i < length; i++) {
@@ -21,6 +35,7 @@ int main() {
       fourth += ' ';
     }
   }
+  
   string third = "* " + greeting + name + " *";
   cout << first << endl << second << endl << third << endl << fourth << endl << fifth;
   return 0;
